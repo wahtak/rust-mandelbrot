2a319b1f47bbe751cdff57803689522cf23ba3d9
Alexander Metz <alexander.wt.metz@gmail.com>
2019-04-22 20:05:51 +0200

    improve naming

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ extern crate num;
 
 use num::complex::Complex;
 
-fn f_c(z: Complex<f64>, c: Complex<f64>) -> Complex<f64> {
+fn mandelbrot_function(z: Complex<f64>, c: Complex<f64>) -> Complex<f64> {
     z * z + c
 }
 
@@ -21,9 +21,9 @@ mod tests {
     use super::*;
 
     #[test]
-    fn f_c_evaluates_correctly_for_complex_numbers() {
+    fn mandelbrot_function_evaluates_correctly_for_complex_numbers() {
         assert_eq!(Complex::new(5.5, 13.),
-            f_c(Complex::new(3., 2.), Complex::new(0.5, 1.)));
+            mandelbrot_function(Complex::new(3., 2.), Complex::new(0.5, 1.)));
     }
 
     #[test]
