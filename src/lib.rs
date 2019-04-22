b220c9ebf9c6c68acc1b03be5433e5b6ef9fd2cf
Alexander Metz <alexander.wt.metz@gmail.com>
2019-04-22 23:07:09 +0200

    refactor for readablility

--- src/lib.rs
+++ src/lib.rs
@@ -18,8 +18,8 @@ impl Bounded for f64 {
     }
 }
 
-fn mandelbrot_function(z: Complex64, c: Complex64) -> Complex64 {
-    z * z + c
+fn mandelbrot_function(c: Complex64) -> Box<Fn(Complex64) -> Complex64> {
+    Box::new(move |z| z * z + c)
 }
 
 fn measure_divergence<T: Bounded + num::Zero>(function: &Fn(T) -> T, bound: f64, max_iterations: i64) -> Option<i64> {
@@ -46,13 +46,12 @@ mod tests {
         let z = Complex64{re: 3.0, im: 2.0};
         let c = Complex64{re: 0.5, im: 1.0};
 
-        assert_eq!(Complex64{re: 5.5, im: 13.}, mandelbrot_function(z, c));
+        assert_eq!(Complex64{re: 5.5, im: 13.}, mandelbrot_function(c)(z));
     }
 
     #[test]
-    fn measure_divergence_counts_iterations_of_function_until_value_reaches_bound() {
-        let c = 1.0;
-        let add_constant = |z: f64| z + c;
+    fn measure_divergence_returns_number_of_iterations_after_which_value_is_out_of_bound() {
+        let add_constant = |z: f64| z + 1.0;
         let bound = 3.5;
 
         let iterations = measure_divergence(&add_constant, bound, 10);
@@ -61,7 +60,7 @@ mod tests {
     }
 
     #[test]
-    fn measure_divergence_outputs_none_when_number_of_iterations_exceeds_maximum() {
+    fn measure_divergence_returns_none_when_number_of_iterations_exceeds_maximum() {
         let negate = |z: f64| z * -1.;
         let max_iterations = 10;
 
@@ -73,9 +72,8 @@ mod tests {
     #[test]
     fn mandelbrot_function_does_not_diverge_for_some_constant() {
         let c = Complex64{re: 0.0, im: 0.0};
-        let function = |z: Complex64| mandelbrot_function(z, c);
 
-        let iterations = measure_divergence(&function, 3.5, 10);
+        let iterations = measure_divergence(&*mandelbrot_function(c), 3.5, 10);
 
         assert!(iterations.is_none());
     }
