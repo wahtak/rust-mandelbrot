1c1603f0e3c8db39ac96e3cf422733b53cca13f6
Alexander Metz <alexander.wt.metz@gmail.com>
2019-04-22 13:41:12 +0200

    modify f_c for use with complex numbers

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
-pub fn f_c(z: f64, c: f64) -> f64 {
+extern crate num;
+
+use num::complex::Complex;
+
+fn f_c(z: Complex<f64>, c: Complex<f64>) -> Complex<f64> {
     z * z + c
 }
 
@@ -7,8 +11,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn f_c_evaulates_correctly_for_real_numbers() {
-        assert_eq!(0., f_c(0., 0.));
-        assert_eq!(5., f_c(2., 1.));
+    fn f_c_evaluates_correctly_for_complex_numbers() {
+        assert_eq!(Complex::new(5.5, 13.),
+            f_c(Complex::new(3., 2.), Complex::new(0.5, 1.)));
     }
 }
