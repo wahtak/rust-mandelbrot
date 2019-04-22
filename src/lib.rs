6dc3b7c1e2bff19be5961d94b6c089d3460cd60c
Alexander Metz <alexander.wt.metz@gmail.com>
2019-04-22 21:04:54 +0200

    refactor tests for readability

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ fn mandelbrot_function(z: Complex<f64>, c: Complex<f64>) -> Complex<f64> {
     z * z + c
 }
 
-fn measure_divergence(function: &Fn(i64) -> i64, initial_value: i64, bound: i64, maximum_iterations: i64) -> Option<i64>
+fn measure_divergence(function: &Fn(i64) -> i64, initial_value: i64, bound: i64, max_iterations: i64) -> Option<i64>
 {
     let mut value = initial_value;
     let mut iterations = 0;
@@ -14,7 +14,7 @@ fn measure_divergence(function: &Fn(i64) -> i64, initial_value: i64, bound: i64,
         if value > bound {
             return Some(iterations);
         }
-        if iterations == maximum_iterations {
+        if iterations == max_iterations {
             return None;
         }
         value = function(value);
@@ -29,22 +29,31 @@ mod tests {
 
     #[test]
     fn mandelbrot_function_evaluates_correctly_for_complex_numbers() {
-        assert_eq!(Complex::new(5.5, 13.),
-            mandelbrot_function(Complex::new(3., 2.), Complex::new(0.5, 1.)));
+        let z = Complex::new(3., 2.);
+        let c = Complex::new(0.5, 1.);
+
+        assert_eq!(Complex::new(5.5, 13.), mandelbrot_function(z, c));
     }
 
     #[test]
     fn measure_divergence_counts_iterations_of_closure_until_value_reaches_bound() {
         let c = 1;
         let add_c = |i| i + c;
-        let iterations = measure_divergence(&add_c, 0, 3, 5);
+        let initial_value = 0;
+        let bound = 3;
+
+        let iterations = measure_divergence(&add_c, initial_value, bound, 5);
+
         assert_eq!(4, iterations.unwrap());
     }
 
     #[test]
     fn measure_divergence_outputs_none_when_number_of_iterations_exceeds_maximum() {
         let negate = |i| i * -1;
-        let iterations = measure_divergence(&negate, 0, 3, 5);
+        let max_iterations = 5;
+
+        let iterations = measure_divergence(&negate, 0, 3, max_iterations);
+
         assert!(iterations.is_none());
     }
 }
