491ced754ae1479fca21e3d7feb7c964fe3e2c39
Alexander Metz <alexander.wt.metz@gmail.com>
2019-04-22 20:56:17 +0200

    handle non-diverging functions

--- src/lib.rs
+++ src/lib.rs
@@ -6,14 +6,21 @@ fn mandelbrot_function(z: Complex<f64>, c: Complex<f64>) -> Complex<f64> {
     z * z + c
 }
 
-fn measure_divergence(func: &Fn(f64) -> f64, initial_value: f64, bound: f64) -> i64 {
+fn measure_divergence(function: &Fn(i64) -> i64, initial_value: i64, bound: i64, maximum_iterations: i64) -> Option<i64>
+{
     let mut value = initial_value;
     let mut iterations = 0;
-    while value < bound {
-        value = func(value);
+    loop {
+        if value > bound {
+            return Some(iterations);
+        }
+        if iterations == maximum_iterations {
+            return None;
+        }
+        value = function(value);
         iterations += 1;
     }
-    iterations
+
 }
 
 #[cfg(test)]
@@ -28,9 +35,16 @@ mod tests {
 
     #[test]
     fn measure_divergence_counts_iterations_of_closure_until_value_reaches_bound() {
-        let x = 1.;
-        let add_x = |n| n + x;
-        let iterations = measure_divergence(&add_x, 0., 3.);
-        assert_eq!(3, iterations);
+        let c = 1;
+        let add_c = |i| i + c;
+        let iterations = measure_divergence(&add_c, 0, 3, 5);
+        assert_eq!(4, iterations.unwrap());
+    }
+
+    #[test]
+    fn measure_divergence_outputs_none_when_number_of_iterations_exceeds_maximum() {
+        let negate = |i| i * -1;
+        let iterations = measure_divergence(&negate, 0, 3, 5);
+        assert!(iterations.is_none());
     }
 }
