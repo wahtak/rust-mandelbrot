e060da9638b655bc474590491a13957006389d60
Alexander Metz <alexander.wt.metz@gmail.com>
2019-04-28 19:16:12 +0200

    add empty draw function

--- src/draw.rs
+++ src/draw.rs
@@ -1,5 +1,11 @@
+extern crate image;
+
 pub fn float_range(min: f64, max: f64, step: f64) -> impl std::iter::Iterator<Item = f64> {
     (((min / step) as i64)..=((max / step) as i64)).map(move |i| i as f64 * step)
+}
+
+pub fn draw() -> image::RgbImage {
+	image::RgbImage::new(10, 10)
 }
 
 #[cfg(test)]
@@ -17,5 +23,12 @@ mod tests {
         assert!(is_near(iterator.next().unwrap(), 0.4));
         assert!(is_near(iterator.next().unwrap(), 0.8));
         assert!(iterator.next().is_none());
+    }
+
+    #[test]
+    fn draw_creates_rbg_image() {
+    	let img : image::RgbImage = draw();
+    	let check_intensity = |img : image::RgbImage, x, y, intensity : u8| *img.get_pixel(x, y) == image::Rgb([intensity; 3]);
+    	assert!(check_intensity(img, 0, 0, 0));
     }
 }
